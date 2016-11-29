d1d5443081f923f2e2828161753502a0f18cc27a
estrellar <student@ssu-cs-dar28-167-220.sonoma.edu>
2016-11-28 17:15:10 -0800

    changed trainModel to work on paths November 28 5:14 PM

--- trainModel.m
+++ trainModel.m
@@ -11,24 +11,24 @@ clear;
 DISPLAY_LEVEL=1;
 DEBUG_FLAG=0;
 
-in_folder='ClassificationAttempt\Model';
-in_folder_pwd=strcat(pwd(), '\', in_folder);
+in_folder='ClassificationAttempt/Model';
+in_folder_pwd=strcat(pwd(), '/', in_folder);
 in_features_base_mat_file='features.mat';
 in_nonFeatures_base_mat_file='nonFeatures.mat';
 in_processedClassColors_mat_file = 'pcc.mat';
-out_folder='ClassificationAttempt\Model';
-out_folder_pwd=strcat(pwd(), '\', out_folder);
+out_folder='ClassificationAttempt/Model';
+out_folder_pwd=strcat(pwd(), '/', out_folder);
 out_model_base_mat_file='model.mat';
 
-temp = load(strcat(in_folder_pwd,'\',in_processedClassColors_mat_file));
+temp = load(strcat(in_folder_pwd,'/',in_processedClassColors_mat_file));
 processedClassColors = temp.processedClassColors;
 for i=1:length(processedClassColors)
     processedColor=processedClassColors(i);
     in_features_mat_file = sprintf('%s_%d.mat', in_features_base_mat_file(1:end-4),processedColor);
     in_nonFeatures_mat_file = sprintf('%s_%d.mat', in_nonFeatures_base_mat_file(1:end-4),processedColor);
-    temp = load(strcat(in_folder_pwd,'\',in_features_mat_file));
+    temp = load(strcat(in_folder_pwd,'/',in_features_mat_file));
     features = temp.features;
-    temp = load(strcat(in_folder_pwd,'\',in_nonFeatures_mat_file));
+    temp = load(strcat(in_folder_pwd,'/',in_nonFeatures_mat_file));
     nonFeatures = temp.nonFeatures;   
     
     if(DISPLAY_LEVEL==1)
@@ -61,7 +61,7 @@ for i=1:length(processedClassColors)
     if(DISPLAY_LEVEL==1)
          fprintf('\n*--- Saving model file %s',out_model_mat_file);
     end
-    save(strcat(out_folder_pwd,'\',out_model_mat_file), 'model');   
+    save(strcat(out_folder_pwd,'/',out_model_mat_file), 'model');   
 end
 
-fprintf('\n');
+fprintf('\n **** Training Done ******\n');
